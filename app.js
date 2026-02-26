083c167a7f7d6de22a165a49a07c7446f062c765
Drakdodare <102174216+Drakdodare@users.noreply.github.com>
2026-02-26 06:56:37 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,40 +2,18 @@ const GAMES = [
   // ===== FREEWARE =====
   {
     name: "Digger (Freeware)",
-    url: "./bundles/digger.jsdos"
-  },
-  {
-    name: "Nibbles (QBasic)",
-    url: "./bundles/nibbles.jsdos"
-  },
-  {
-    name: "ZZT",
-    url: "./bundles/zzt.jsdos"
-  },
-
-  // ===== SHAREWARE (legal to host shareware episodes only) =====
-  {
-    name: "Commander Keen Ep1 (Shareware)",
-    url: "./bundles/keen1.jsdos"
+    url: "bundles/digger.jsdos"
   },
   {
     name: "Doom v1.9 (Shareware)",
-    url: "./bundles/doom-shareware.jsdos"
+    url: "bundles/doom.jsdos"
   },
   {
     name: "Wolfenstein 3D (Shareware)",
-    url: "./bundles/wolf3d-shareware.jsdos"
+    url: "bundles/wolf3d.jsdos"
   },
   {
     name: "Duke Nukem 3D (Shareware)",
-    url: "./bundles/duke3d-shareware.jsdos"
+    url: "bundles/duke3d.jsdos"
   },
-  {
-    name: "One Must Fall 2097 (Shareware)",
-    url: "./bundles/omf2097-shareware.jsdos"
-  },
-  {
-    name: "Liero",
-    url: "./bundles/liero.jsdos"
-  }
 ];
