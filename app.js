ca985392066d110ee08e11c89b704e4290fd69e1
Drakdodare <102174216+Drakdodare@users.noreply.github.com>
2026-02-26 06:58:39 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
 const GAMES = [
-  // ===== FREEWARE =====
-  {
-    name: "Digger (Freeware)",
-    url: "bundles/digger.jsdos"
-  },
   {
     name: "Doom v1.9 (Shareware)",
     url: "bundles/doom.jsdos"
-  },
-  {
-    name: "Wolfenstein 3D (Shareware)",
-    url: "bundles/wolf3d.jsdos"
-  },
-  {
-    name: "Duke Nukem 3D (Shareware)",
-    url: "bundles/duke3d.jsdos"
-  },
+  }
 ];
